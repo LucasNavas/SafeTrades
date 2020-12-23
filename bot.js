19b836f4797d56b0f3e7ce8e3039fe50fd1c8965
LucasNavas <lucas-navas1@hotmail.com>
2020-12-23 16:32:48 -0300

    Novo package + login automatico + mensagem de resposta
    Adicionado package steamtotp na package.json
    
    Login automatico na config.json com steam-totp(oculta)
    
    Mensagem de resposta do bot ao usuário

--- bot.js
+++ bot.js
@@ -1,14 +1,22 @@
-const SteamUser = require('steam-user');
-const config = require('./config.json');
-const client = new SteamUser();
+const SteamUser = require('steam-user')
+const SteamTotp = require('steam-totp')
+const config = require('./config.json')
+const client = new SteamUser()
 const logOnOptions = {
     accountName: config.nome,
-    password: config.senha
-};
+    password: config.senha,
+    twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
+}
 
-client.logOn(logOnOptions);
+client.logOn(logOnOptions)
 client.on('loggedOn', () => {
     console.log('Conectado com sucesso.')
-    client.setPersona(SteamUser.EPersonaState.Away);
-    client.gamesPlayed('bolsonaro gostosa', 440);
-});
+    client.setPersona(SteamUser.EPersonaState.Away)
+    client.gamesPlayed("Contra Ataque: Global Ofensivo",730);
+})
+
+client.on("friendMessage", function(steamID, message) {
+    if (message == "ola") {
+        client.chatMessage(steamID, "Olá viadao, quer oq??")
+    }
+})
