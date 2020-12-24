74f8fab4fc89a8ce9a18df7504a9b5fd07070ca3
LucasNavas <lucas-navas1@hotmail.com>
2020-12-24 10:10:05 -0300

    Atualização de trade (incompleta)
    Adicionado bibliotecas de tradeoffer (enviar ereceber ofertas de troca) e de community (necessária para os cookies e verificação de confirmação do steam guard)

--- bot.js
+++ bot.js
@@ -1,7 +1,17 @@
 const SteamUser = require('steam-user')
 const SteamTotp = require('steam-totp')
+const SteamCommunity = require('steamcommunity')
+const TradeOfferManager = require('steam-tradeoffer-manager')
 const config = require('./config.json')
+
 const client = new SteamUser()
+const community = new SteamCommunity()
+const manager = new TradeOfferManager({
+    steam: client,
+    community: community,
+    language: 'br'
+})
+
 const logOnOptions = {
     accountName: config.nome,
     password: config.senha,
@@ -9,6 +19,7 @@ const logOnOptions = {
 }
 
 client.logOn(logOnOptions)
+
 client.on('loggedOn', () => {
     console.log('Conectado com sucesso.')
     client.setPersona(SteamUser.EPersonaState.Away)
@@ -18,5 +29,31 @@ client.on('loggedOn', () => {
 client.on("friendMessage", function(steamID, message) {
     if (message == "ola") {
         client.chatMessage(steamID, "Olá viadao, quer oq??")
+    }
+})
+
+client.on('WebSession', (sessionid, cookies) =>{
+    manager.setCookies(cookies)
+    community.setCookies(cookies)
+    community.startConfirmationChecker(10000, config.identifiySecret)
+    
+})
+function acceptOffer(offer){
+    offer.accept((err) => {
+        if (err) console.log("Não foi possível aceitar a troca.")
+    })
+}
+function declineOffer(offer){
+    offer.decline((err) => {
+        if (err) console.log("Não foi possível recusar a troca.")
+    })
+}
+
+client.setOption("promptSteamGuardCode", false)
+manager.on('newOffer',(offer) => {
+    if(offer.partner.getSteamID64() === config.ownerID) {
+        acceptOffer(offer)
+    } else {
+        declineOffer(offer)
     }
 })
